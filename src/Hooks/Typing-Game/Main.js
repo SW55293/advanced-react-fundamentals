81ce46b0c4311f6f02588178ffd2aefd64865cdd
Stephanie <yesstephanie1@gmail.com>
2021-08-19 21:46:54 -0700

    use ref

--- src/Hooks/Typing-Game/Main.js
+++ src/Hooks/Typing-Game/Main.js
@@ -1,63 +1,64 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Main = () => {
-	
+
 	const [text, setText] = useState("")
 	const [timeLeft, setTimeLeft] = useState(5)
 	const [isTimeRunning, setTimeRunning] = useState(false)
 	const [countedWordz, setCountedWordz] = useState(0)
 
 	const handleChange = (e) => {
-		const {value} = e.target
+		const { value } = e.target
 		setText(value)
 	}
-	
-	function WordCount(text) { 
-		const words = text.trim().split(" ") 
+
+	function WordCount(text) {
+		const words = text.trim().split(" ")
 		//console.log(words)
 		return words.filter(word => word !== "").length
-	  }
+	}
 
-	  function startGame() {
-		  setTimeRunning(true)
-		  setTimeLeft(5)
-		  setText("")
-	  }
+	function startGame() {
+		setTimeRunning(true)
+		setTimeLeft(5)
+		setText("")
+	}
 
-	  function endOfGame(text) {
+
+	// the following use effect controls the countdown once clicking start
+	useEffect(() => {
+		if (isTimeRunning === true && timeLeft > 0) {
+			setTimeout(() => {
+				setTimeLeft(time => time - 1)
+			}, 1000)
+		} else if (isTimeRunning === 0) {
+			endOfGame()
+		}
+		// eslint-disable-next-line
+	}, [timeLeft, isTimeRunning])
+
+	const endOfGame = () => {
 		setTimeRunning(false)
 		const numWordz = WordCount(text)
 		setCountedWordz(numWordz)
 		//or
 		//setCountedWordz(WordCount(text))
 
-	  }
-	// the following use effect controls the countdown once clicking start
-	useEffect(() => {
-		if(isTimeRunning === true && timeLeft > 0) {
-			setTimeout(() => {
-				setTimeLeft(time => time - 1)
-			}, 1000)
-		} else if(isTimeRunning === 0) {	
-			endOfGame()
-		}
-	}, [timeLeft, isTimeRunning])
-	  
-
+	}
 	//console.log(text)
 	return (
 		<div>
 			<h1>How fast do you type?</h1>
-			<textarea 
+			<textarea
 				onChange={handleChange}
 				value={text}
 				disabled={!isTimeRunning}
-			
+
 			/>
 			<h4>Time Remaining: {timeLeft} seconds</h4>
-			<button 
-			onClick={startGame}
-			disabled={isTimeRunning}
+			<button
+				onClick={startGame}
+				disabled={isTimeRunning}
 			>
 				Start</button>
 			<h1>Word Count: {countedWordz}</h1>
