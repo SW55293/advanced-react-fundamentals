8a24d7df056f17395b0780aeff0d8e774c4af230
Stephanie <yesstephanie1@gmail.com>
2021-08-23 20:51:14 -0700

    switch and route

---
+++ src/Router/Link-Route-Switch/App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {Link, Switch, Route} from 'react-router-dom'
+
+import Home from './Home'
+import About from './About'
+import Profile from './Profile'
+
+function App() {
+	return (
+		<div>
+			<Link to="/">Home Page</Link>
+			<Link to="/about">About Page</Link>
+			<Link to="/profile">Profile Page</Link>
+
+			<Switch>
+				<Route exact path="/"> <Home /> </Route>
+				<Route path="/about"> <About /> </Route>
+				<Route path="/profile"> <Profile /> </Route>
+			</Switch>
+			
+		</div>
+	)
+
+}
+
+export default App
+
+
+
+/*
+one way to pass components in the route
+use the below if you need to pass props
+
+----Version 1
+<Route exact path="/" render={() => <Home />} />
+<Route path="/about" render={() => <About />} />
+<Route path="/profile" render={() => <Profile />} />
+
+----Version 2
+<Route exact path="/" component={Home} />
+<Route path="/about" component={About} />
+<Route path="/profile" component={Profile} />
+
+----Version 3
+<Route exact path="/"> <Home /> </Route>
+<Route path="/about"> <About /> </Route>
+<Route path="/profile"> <Profile /> </Route>
+
+
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
+import App from './Router/Link-Route-Switch/App'
 
 
 ReactDOM.render(
 	<Router>
-	<App />
+		<App />
 	</Router>,
 	document.getElementById("root")
 )
