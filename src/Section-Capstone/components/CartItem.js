dfaa4fa164a4965a2ad9e0ddd86b4a00529b6183
Stephanie <yesstephanie1@gmail.com>
2021-09-05 22:01:56 -0700

    trash bin on hover styling

--- src/Section-Capstone/components/CartItem.js
+++ src/Section-Capstone/components/CartItem.js
@@ -1,7 +1,12 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import {Context} from '../Context'
 
+//need to have some defualts so we use prop types
+import PropTypes from 'prop-types'
+
 function CartItem({item}) {
+	const [hover, setHover] = useState(false)
+	const trashHover = hover ? "ri-delete-bin-fill" : "ri-delete-bin-line"
 
 	//brought in functions from the context page to use here
 	const {removeFromCart} = useContext(Context)
@@ -9,12 +14,23 @@ function CartItem({item}) {
 
 	return (
 		<div className="cart-item">
-			<i className="ri-delete-bin-line" onClick={() => removeFromCart(item.id)}></i>
-			<img src={item.url} width="120px" alt="cart-item"/>
+			<i 
+			className={trashHover}
+			onClick={() => removeFromCart(item.id)}
+			onMouseEnter={() => setHover(true)} 
+			onMouseLeave={() => setHover(false)}
+			></i>
+			<img src={item.url} width="120px" alt="cart-item" />
 			<p>$2.99</p>
 
 		</div>
 	)
+}
+
+CartItem.propType = {
+	item: PropTypes.shape({
+		url: PropTypes.string.isRequired
+	})
 }
 
 export default CartItem
