b5a1b951693673b62bc312f46082d58d8084f937
Stephanie <yesstephanie1@gmail.com>
2021-09-05 21:12:00 -0700

    added total cost and removing imgs

--- src/Section-Capstone/pages/Cart.js
+++ src/Section-Capstone/pages/Cart.js
@@ -4,16 +4,22 @@ import CartItem from '../components/CartItem'
 
 function Cart() {
 	const {cartItems} = useContext(Context)
+
+	const costOfEach = 2.99 * cartItems.length
+	const totalCost = costOfEach.toLocaleString("en-US", {style: "currency", currency: "USD" })
+
 	const cartItemElements = cartItems.map(item =>(
 		<CartItem key={item.id} item={item}/>
 	))
+
+	
 	
     return (
         <main className="cart-page">
             <h1>Check out</h1>
 			{cartItemElements}
 
-			<p className="total-cost">Total: </p>
+			<p className="total-cost">Total: {totalCost}</p>
 
 			<div className="order-button">
 				<button>Place Order</button>
